7e551adafa22224e310f95d1bf53d627cc6a6063
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-08-27 15:45:36 +0200

    feat: db context

---
+++ src/AsyncCsvProcessor.Application/IAsyncCsvProcessorDbContext.cs
@@ -0,0 +1,10 @@
+﻿using AsyncCsvProcessor.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncCsvProcessor.Application;
+
+public interface IAsyncCsvProcessorDbContext
+{
+    DbSet<Job> Jobs { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/AsyncCsvProcessor.Infrastructure/AsyncCsvProcessorDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AsyncCsvProcessor.Application;
+using AsyncCsvProcessor.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncCsvProcessor.Infrastructure;
+
+public class AsyncCsvProcessorDbContext : DbContext, IAsyncCsvProcessorDbContext
+{
+    public AsyncCsvProcessorDbContext(DbContextOptions<AsyncCsvProcessorDbContext> options) 
+        : base(options) { }
+    
+    public DbSet<Job> Jobs => Set<Job>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Job>(entity =>
+        {
+            entity.HasKey(j => j.Id);
+            entity.Property(j => j.FileName).IsRequired().HasMaxLength(260);
+            entity.Property(j => j.Status).HasConversion<string>();
+        });
+    }
+}
