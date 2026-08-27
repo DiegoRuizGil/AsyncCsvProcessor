8e3e46f5fdbfe2ae6cba158df82e67bad045ddd4
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-08-27 15:59:00 +0200

    feat: job post and get

--- src/AsyncCsvProcessor.Api/Program.cs
+++ src/AsyncCsvProcessor.Api/Program.cs
@@ -1,41 +1,32 @@
+using AsyncCsvProcessor.Application;
+using AsyncCsvProcessor.Domain;
+using AsyncCsvProcessor.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AsyncCsvProcessorDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+builder.Services.AddScoped<IAsyncCsvProcessorDbContext>(sp =>
+    sp.GetRequiredService<AsyncCsvProcessorDbContext>());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapPost("/jobs", async (CreateJobRequest request, IAsyncCsvProcessorDbContext db, CancellationToken ct) =>
 {
-    app.MapOpenApi();
-}
+    var job = new Job(request.FileName);
+    db.Jobs.Add(job);
+    await db.SaveChangesAsync(ct);
+    return Results.Created($"/jobs/{job.Id}", new { job.Id, job.Status });
+});
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/jobs/{id:guid}", async (Guid id, IAsyncCsvProcessorDbContext db, CancellationToken ct) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var job = await db.Jobs.FindAsync([id], ct);
+    return job is null ? Results.NotFound() : Results.Ok(job);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+record CreateJobRequest(string FileName);
