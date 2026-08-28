5e9e22d0d27c32b56e7069774a87b3ad0a0c1743
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-08-28 11:03:48 +0200

    feat: consume job submitted messages

---
+++ src/AsyncCsvProcessor.Worker/Consumer/JobSubmittedConsumer.cs
@@ -0,0 +1,41 @@
+﻿using AsyncCsvProcessor.Application;
+using MassTransit;
+
+namespace AsyncCsvProcessor.Worker.Consumer;
+
+public class JobSubmittedConsumer : IConsumer<JobSubmitted>
+{
+    private readonly IAsyncCsvProcessorDbContext _db;
+    private readonly ILogger<JobSubmittedConsumer> _logger;
+
+    public JobSubmittedConsumer(IAsyncCsvProcessorDbContext db, ILogger<JobSubmittedConsumer> logger)
+    {
+        _db = db;
+        _logger = logger;
+    }
+    
+    public async Task Consume(ConsumeContext<JobSubmitted> context)
+    {
+        var message = context.Message;
+        var job = await _db.Jobs.FindAsync([message.JobId], context.CancellationToken);
+
+        if (job == null)
+        {
+            _logger.LogWarning("Job {jobId} not found, the message is discarded", message.JobId);
+            return;
+        }
+        
+        job.MarkAsProcessing();
+        await _db.SaveChangesAsync(context.CancellationToken);
+        _logger.LogInformation("Job {JobId} marked as Processing", message.JobId);
+        
+        // TODO - parse CSV
+        // just for debugging
+        var totalRows = 100;
+        var processedRows = 100;
+        
+        job.MarkAsCompleted(totalRows, processedRows);
+        await _db.SaveChangesAsync(context.CancellationToken);
+        _logger.LogInformation("Job {JobId} marked as {Status}", job.Id, job.Status);
+    }
+}
--- src/AsyncCsvProcessor.Worker/Program.cs
+++ src/AsyncCsvProcessor.Worker/Program.cs
@@ -1,7 +1,33 @@
+using AsyncCsvProcessor.Application;
+using AsyncCsvProcessor.Infrastructure;
 using AsyncCsvProcessor.Worker;
+using AsyncCsvProcessor.Worker.Consumer;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddDbContext<AsyncCsvProcessorDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+builder.Services.AddScoped<IAsyncCsvProcessorDbContext>(sp =>
+    sp.GetRequiredService<AsyncCsvProcessorDbContext>());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<JobSubmittedConsumer>();
+    
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"] ?? "localhost", "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMq:Username"] ?? "guest");
+            h.Password(builder.Configuration["RabbitMq:Password"] ?? "guest");
+        });
+        
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var host = builder.Build();
 host.Run();
