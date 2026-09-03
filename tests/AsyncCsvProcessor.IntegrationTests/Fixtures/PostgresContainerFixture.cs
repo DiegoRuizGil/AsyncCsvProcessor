b760cf42e2ba02d68f1686f2decf7395b9654042
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-09-03 23:54:06 +0200

    feat: integrate Respawn in PostgresContainerFixture

--- tests/AsyncCsvProcessor.IntegrationTests/Fixtures/PostgresContainerFixture.cs
+++ tests/AsyncCsvProcessor.IntegrationTests/Fixtures/PostgresContainerFixture.cs
@@ -1,5 +1,7 @@
 ﻿using AsyncCsvProcessor.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Respawn;
 using Testcontainers.PostgreSql;
 
 namespace AsyncCsvProcessor.IntegrationTests.Fixtures;
@@ -7,7 +9,11 @@ namespace AsyncCsvProcessor.IntegrationTests.Fixtures;
 public class PostgresContainerFixture : IAsyncLifetime
 {
     private readonly PostgreSqlContainer _container;
+    private NpgsqlConnection _respawnConnection = null!;
+    private Respawner _respawner = null!;
 
+    public string ConnectionString => _container.GetConnectionString();
+    
     public PostgresContainerFixture()
     {
         _container = new PostgreSqlBuilder("postgres:17")
@@ -23,9 +29,23 @@ public class PostgresContainerFixture : IAsyncLifetime
 
         await using var dbContext = CreateDbContext();
         await dbContext.Database.MigrateAsync();
+
+        _respawnConnection = new NpgsqlConnection(ConnectionString);
+        await _respawnConnection.OpenAsync();
+        
+        _respawner = await Respawner.CreateAsync(_respawnConnection, new RespawnerOptions
+        {
+            SchemasToInclude = ["public"],
+            TablesToIgnore = ["__EFMigrationsHistory"],
+            DbAdapter = DbAdapter.Postgres
+        });
     }
 
-    public async Task DisposeAsync() => await _container.DisposeAsync();
+    public async Task DisposeAsync()
+    {
+        await _respawnConnection.DisposeAsync();
+        await _container.DisposeAsync();
+    }
 
     public AsyncCsvProcessorDbContext CreateDbContext()
     {
@@ -35,4 +55,6 @@ public class PostgresContainerFixture : IAsyncLifetime
 
         return new AsyncCsvProcessorDbContext(options);
     }
+
+    public Task ResetAsync() => _respawner.ResetAsync(_respawnConnection);
 }
